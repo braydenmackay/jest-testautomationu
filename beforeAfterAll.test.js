8849e84179824acdb5e388b42792ef23c3051fa5
braydenmackay <braydenmackay1@gmail.com>
2019-11-17 17:49:23 -0700

    initial commit

---
+++ beforeAfterAll.test.js
@@ -0,0 +1,17 @@
+const setup = () => console.log("Setting up before test run")
+const tearDown = () => console.log("Finish up after tests run")
+
+describe("new account creation checks", () => {
+  beforeAll(() => setup())
+  afterAll(() => tearDown())
+
+  test("new account 1 created", () => {
+    const account = "account1"
+    expect(account).toEqual("account1")
+  })
+
+  test("new account 2 created", () => {
+    const account = "account2"
+    expect(account).toEqual("account2")
+  })
+})
